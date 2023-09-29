d30827fba17c24b16cdd98098ac95ace358f5d62
tomoR2 <144089090+tomoR2@users.noreply.github.com>
2023-09-29 20:09:25 +0900

    ベーシック認証、beforeアクション追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-
+  before_action :basic_auth
 
 
 
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
     end
   end
 end
