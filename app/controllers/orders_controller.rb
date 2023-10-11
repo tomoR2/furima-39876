a04f9d83d831f66e1ae3b9a5832f1ed1cb2a97d4
tomoR2 <144089090+tomoR2@users.noreply.github.com>
2023-10-11 20:55:52 +0900

    OrderAddressテーブル、カラム、バリデーション、save追記

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.create(order_params)
+    Address.create(address_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params.permit(:post_code, :prefecture, :municipality, :street_address, :phone_number).merge(order_id: @order.id)
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,22 +1,30 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, 
-  :street_address, :building_name, :phone_number, :order_id
+  :street_address, :building_name, :phone_number
 
   with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
+  #  orderのバリデーション
     validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :item_id
 
-    validates :item_name, :concept, :image, presence: true
-    validates :price, numericality: {
-      only_integer: true,
-      greater_than_or_equal_to: 300,
-      less_than_or_equal_to: 9_999_999,
-      message: '半角数字300～9999999で設定ください'
-    }
-    validates :category_id, :condition_id, :delivery_charge_id,
-              :delivery_days_id, :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  #  addressのバリデーション
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :municipality
+    validates :street_address
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+
+    #  tokenのバリデーションあとでつかう
+    # validates :token
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
-end
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(
+      post_code: post_code, prefecture_id: prefecture.id, municipality: municipality, street_address: street_address, 
+      phone_number: phone_number, building_name: building_name, order_id: order.id )
+  end
+
+
+
