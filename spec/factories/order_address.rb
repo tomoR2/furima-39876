68a289fb0fb08d80f5add990ed6729c4c3eec0a8
tomoR2 <144089090+tomoR2@users.noreply.github.com>
2023-10-12 21:43:15 +0900

    Formオブジェクト、配送先情報テストコード

---
+++ spec/factories/order_address.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_address do
+    post_code { "123-4567" }
+    prefecture_id { 2 }
+    municipality { "石巻" }
+    street_address { "民芸調" }
+    building_name { "花山ビル" }
+    phone_number { "09012345678" }
+
+    end
+end
---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,88 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)  
+    end
+
+  describe '商品購入できる場合(決済以外)' do
+    # 正常系テスト
+    context '商品購入できる場合' do
+      it '購入の必須情報が存在すれば登録できる' do
+        expect(@order_address).to be_valid
+      end
+    end
+
+    # 異常系テスト
+    context '商品購入できない場合' do
+      it '郵便番号の登録が空' do
+        @order_address.post_code = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+      end
+
+      it '郵便番号の登録が全角' do
+        @order_address.post_code = "１２３-４５６７"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+      end
+
+      it '郵便番号の登録が-なし' do
+        @order_address.post_code = "1234567"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+      end
+
+      it '郵便番号の桁数が3行ハイフン4行じゃない' do
+        @order_address.post_code = "1-234567"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+      end
+
+      it '都道府県の登録が---' do
+        @order_address.prefecture_id = '1'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '市区町村の登録が空' do
+        @order_address.municipality = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Municipality can't be blank")
+      end
+
+      it '番地の登録が空' do
+        @order_address.street_address = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Street address can't be blank")
+      end
+
+      it '電話番号の登録が空' do
+        @order_address.phone_number = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it '電話番号の登録が全角' do
+        @order_address.phone_number = "０９０１２３４５６７８"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+
+      it '電話番号の登録が-あり' do
+        @order_address.phone_number = "090-1234-5678"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+
+      it '電話番号の桁数が足りない' do
+        @order_address.phone_number = "090-123-567"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+      end
+
+    end
+  end
+end
