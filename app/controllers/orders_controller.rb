8d83b16f416211f88eb49c8bc85996d8fddfa1e9
tomoR2 <144089090+tomoR2@users.noreply.github.com>
2023-10-12 17:59:06 +0900

    購入画面遷移、空登録エラー表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,30 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
 
   def index
-  end
-
-  def new
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order = Order.create(order_params)
-    Address.create(address_params)
-    redirect_to root_path
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+       @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
 
   def order_params
-    params.permit(:item_id).merge(user_id: current_user.id)
+    params.require(:order_address)
+    .permit(:post_code, :prefecture_id, :municipality, :street_address, :building_name, :phone_number)
+    .merge(user_id: current_user.id, item_id: @item.id)
   end
 
-  def address_params
-    params.permit(:post_code, :prefecture, :municipality, :street_address, :phone_number).merge(order_id: @order.id)
+  def set_item
+    @item = Item.find(params[:item_id])
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items
-  resources :users
-  resources :orders, only: [:index, :new, :create]
+  resources :items do
+   resources :orders, only: [:index, :create]
+  end
 end
